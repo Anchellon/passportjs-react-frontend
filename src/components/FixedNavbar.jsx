87cd4716e9e9a3fc3d86eadd750022228b79a7f0
anchellon <anshulmathew@gmail.com>
2024-01-23 07:28:29 -0800

    Basic Route template for dummy user

--- src/components/FixedNavbar.jsx
+++ src/components/FixedNavbar.jsx
@@ -1,8 +1,11 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import { NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function FixedNavbar() {
+    let loggedIn = false;
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -13,7 +16,30 @@ export default function FixedNavbar() {
                 <Nav className="ms-auto">
                     <Nav.Link href="#home">Link 2</Nav.Link>
                     {/* <Nav.Link href="#features">Features</Nav.Link> */}
-                    <Nav.Link href="#pricing">UserPersona</Nav.Link>
+                    {!loggedIn && (
+                        <Nav.Link href="#features">
+                            <Link className="link" to="login">
+                                Login
+                            </Link>{" "}
+                        </Nav.Link>
+                    )}
+                    {loggedIn && (
+                        <NavDropdown title="{User}" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                Profile
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                Something
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    )}
                 </Nav>
             </Container>
         </Navbar>
